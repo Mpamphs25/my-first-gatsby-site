2fb4f7bb800e61ae70b919775b3d5dd6ce77f9a6
Sentis Mpampis <m.sentis@star.gr>
2022-10-21 13:27:37 +0300

    Finished Gatsby Tutorial Part 3

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {Link} from 'gatsby';
-import {container,heading,navLinks,navLinkItem,navLinkText} from './layout.module.css'
+import {container,heading,navLinks,
+    navLinkItem,navLinkText} from './layout.module.css'
+
 const Layout = ({pageTitle,children}) =>{
     return (
         <div className={container} >
